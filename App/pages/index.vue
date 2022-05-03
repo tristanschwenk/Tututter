3b54cbf029b354c9a7dba75b6c4f75c10fd55bfe
Tristan264 <tristan.bschwenk@icloud.com>
2022-05-03 19:41:22 +0200

    feat(App): add tweets feed and create tweets

--- App/pages/index.vue
+++ App/pages/index.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>
-    <h1>Hello there</h1>
+  <div class="page-wrapper">
+    <div class="container-xl">
+      <h1>Fil d'actualité</h1>
+    </div>
 
-    <ul>
-      <li v-for="example in examples" :key="example.id">{{example.name}}</li>
-    </ul>
+    <div class="page-body">
+      <div class="container-xl">
+        <ul>
+          <li v-for="example in examples" :key="example.id">
+            {{ example.name }}
+          </li>
+        </ul>
+
+        <div class="mb-4">
+          <textarea
+            class="form-control mb-2"
+            name="example-textarea-input"
+            rows="6"
+            placeholder="Racontez quelque chose !"
+            ref="tweetContent"
+          ></textarea>
+          <div class="d-flex justify-content-end">
+            <button class="btn btn-primary" @click.prevent="sendTweet">Postez !</button>
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <div v-for="tweet in tweets" :key="tweet.id" class="card mb-3">
+            <div class="card-body">
+              <h3 class="card-title">{{tweet.user.email}}</h3>
+              <p class="text-muted">
+                {{tweet.content}}
+              </p>
+            </div>
+            <div class="card-footer">This is standard card footer</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,16 +46,29 @@ import { useContext, useFetch, ref } from "@nuxtjs/composition-api";
 
 export default {
   setup() {
-    const { $axios } = useContext();
-    
-     const examples = ref('')
+    const { $axios, $auth } = useContext();
+
+    const examples = ref("");
+    const tweets = ref("");
+    const tweetContent = ref("")
 
     const { fetch, fetchState } = useFetch(async () => {
-      examples.value = await $axios.$get('/examples')
-    })
+      examples.value = await $axios.$get("/examples");
+      tweets.value = await $axios.$get("/tweets");
+    });
 
+    const sendTweet = async () => {
+      const data = {
+        content: tweetContent.value.value,
+        user_id: $auth.$state.user.id
+      }
 
-    return { examples }
+      await $axios.$post("/tweets", data)
+
+      fetch()
+    }
+
+    return { examples, tweets, tweetContent, sendTweet };
   },
 };
 </script>
